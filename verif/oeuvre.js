7a149e48a60fd5fd1c5bbc64291af3e0a4d1bf26
bertreux <115898406+bertreux@users.noreply.github.com>
2023-10-08 21:54:32 +0200

    css page

--- verif/oeuvre.js
+++ verif/oeuvre.js
@@ -4,7 +4,7 @@ import JOI from "joi";
 export const schemaOeuvre = JOI.object({
     image: JOI.string().min(3).max(10000).required(),
     nom: JOI.string().min(3).max(255).required(),
-    auteur: JOI.string().min(3).max(255).required(),
+    auteur: JOI.string().email({ tlds: { allow: false } }).min(3).max(255).required(),
     description: JOI.string().min(3).max(10000).required(),
     dt_creation: JOI.date().required()
 });
